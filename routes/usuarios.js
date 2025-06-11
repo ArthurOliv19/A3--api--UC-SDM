b1de006724963231cbaa98e587541009f6b5675e
ArthurOliveira <114706874+ArthurOliv19@users.noreply.github.com>
2025-06-11 19:54:12 -0300

    usuarios.js

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,9 +6,10 @@ const usuariosController = require('../controllers/Usuario');
 router.get('/', usuariosController.listarUsuarios);
 router.get('/:id', usuariosController.buscarUsuarioPorId);
 router.post('/', usuariosController.criarUsuario);
-router.put('/:id', usuariosController.atualizarUsuario);
-router.patch('/:id', usuariosController.atualizarParcialUsuario);
+router.put('/:id', usuariosController.atualizarUsuario);       
+router.patch('/:id', usuariosController.atualizarParcialUsuario); 
 router.delete('/:id', usuariosController.deletarUsuario);
+
 
 
 
